a20bdfc7d87873e50df8affa54a68898ee59ef9c
J-Bakuli <julia.bakuli@yandex.ru>
2022-06-01 08:54:21 +0300

    Refactoring paths to folder and files

--- src/main/java/org/example/NalogWebScraper.java
+++ src/main/java/org/example/NalogWebScraper.java
@@ -7,10 +7,14 @@ import static org.example.XmlProcessing.xmlProcessing;
 import static org.example.ZipFileUtils.*;
 
 public class NalogWebScraper {
+    /*url - url для парсинга;
+    extractPath - основной путь, куда будет сохранен архив данных после парсинга;
+    downloadPath - путь с названием скачанного архива;
+    csvPath - путь с названием обработанных данных в виде csv файла*/
     static final String url = "http://data.nalog.ru/opendata/7707329152-rafp/";
-    static final String downloadPath = "D://rafp_data//rafp_data.zip"; //Change to your destination
-    static final String extractPath = "D://rafp_data"; //Change to your destination
-    static final String csvPath = "D://rafp_data/rafp_data.csv"; //Change to your destination
+    static final String extractPath = "D://rafp_data"; //Поменять на нужный путь к папке
+    static final String downloadPath = extractPath + "//rafp_data.zip";
+    static final String csvPath = extractPath + "/rafp_data.csv";
 
     public static void main(String[] args) throws IOException {
         downloadZip(findFinalUrl(url), downloadPath);
