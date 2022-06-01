46b02ba46f539f08279c9a7ebadb6c2a61025e7d
J-Bakuli <julia.bakuli@yandex.ru>
2022-06-01 05:17:15 +0300

    Reformat code in main method

--- src/main/java/org/example/NalogWebScraper.java
+++ src/main/java/org/example/NalogWebScraper.java
@@ -1,37 +1,21 @@
 package org.example;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-
 import java.io.IOException;
 
+import static org.example.FinalUrlFinder.findFinalUrl;
+import static org.example.XmlProcessing.xmlProcessing;
+import static org.example.ZipFileUtils.*;
+
 public class NalogWebScraper {
-    public static void main(String[] args) {
+    static final String url = "http://data.nalog.ru/opendata/7707329152-rafp/";
+    static final String downloadPath = "D://rafp_data//rafp_data.zip"; //Change to your destination
+    static final String extractPath = "D://rafp_data"; //Change to your destination
+    static final String csvPath = "D://rafp_data/rafp_data.csv"; //Change to your destination
 
-        String url = "http://data.nalog.ru/opendata/7707329152-rafp/";
-        //List<CompaniesExportData> companies = new LinkedList<>();
-        String urlLastData = null;
-
-        try {
-            Document page = Jsoup.connect(url).get();
-            Elements elementsContainer = page.getElementsByClass("border_table");
-            Elements rows = elementsContainer.select("tr");
-
-            for (int i = 1; i < rows.size(); i++) {
-                Element row = rows.get(i);
-                Elements cols = row.select("td");
-
-                for (Element col : cols) {
-                    if (col.text().equals("Гиперссылка (URL) на набор")) {
-                        urlLastData = col.nextElementSibling().text();
-                    }
-                }
-            }
-            System.out.println(urlLastData);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public static void main(String[] args) throws IOException {
+        downloadZip(findFinalUrl(url), downloadPath);
+        extractAllFilesFromZip(downloadPath, extractPath);
+        String lastVersionXmlPath = findExtractedFilePath(extractPath);
+        xmlProcessing(lastVersionXmlPath, csvPath);
     }
 }
